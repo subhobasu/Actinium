0c2688334850873376282f9964977cbddab55c60
Matthias Kovatsch <kovatsch@inf.ethz.ch>
2012-10-16 23:49:09 +0200

    Prettied up helloworld.js.

--- appserver/installed/helloworld.js
+++ appserver/installed/helloworld.js
@@ -1,1 +1,5 @@
-app.root.onget = function(request) { request.respond(69,"Hello World"); }
+// a handler for GET requests to "/"
+app.root.onget = function(request) {
+// that returns CoAP's "2.05 Content" with payload
+		request.respond(2.05, "Hello world");
+	};
